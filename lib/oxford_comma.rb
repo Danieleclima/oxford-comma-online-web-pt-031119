450ae787f0d7bf524cb06db483d6a0afa9d609da
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-19 20:35:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,18 @@ require "pry"
 def oxford_comma(array)
 last_element = array.last
 if array.size == 1
+<<<<<<< HEAD
   return array.join
 elsif array.size == 2
 return array.join(" and ")
 else
 array.pop
 return array.join(", ") + ", and " + last_element
+=======
+  return array
+else
+array.pop
+return array.join(", ") + " and " + last_element
+>>>>>>> f6e384a11fdee6d07ee49e48e253d3f92056b5db
 end
 end
